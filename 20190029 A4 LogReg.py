ff88e8002c79b972703ec094436a34f1c77b852e
YousefAlTaherr <yousefaltaherr@gmail.com>
2024-05-08 16:29:57 +0300

    Add files via upload

---
+++ 20190029 A4 LogReg.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, roc_auc_score, accuracy_score
+from imblearn.over_sampling import SMOTE
+
+data = pd.read_csv('Dataset.csv').drop_duplicates()
+
+X = data.drop('Diabetes_012', axis=1)
+y = data['Diabetes_012']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.34, random_state=42)
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+model_logistic = LogisticRegression(max_iter=1000)
+
+model_logistic.fit(X_train_scaled, y_train)
+train_predictions = model_logistic.predict(X_train_scaled)
+test_predictions = model_logistic.predict(X_test_scaled)
+train_accuracy = accuracy_score(y_train, train_predictions)
+test_accuracy = accuracy_score(y_test, test_predictions)
+
+print("Logistic Regression without Oversampling")
+print("Train Accuracy:", train_accuracy)
+print("Test Accuracy:", test_accuracy)
+print(classification_report(y_test, test_predictions))
+print("ROC-AUC:", roc_auc_score(y_test, model_logistic.predict_proba(X_test_scaled), multi_class='ovr'))
+
+# with oversampling
+smote = SMOTE()
+X_train_smote, y_train_smote = smote.fit_resample(X_train_scaled, y_train)
+model_logistic.fit(X_train_smote, y_train_smote)
+train_predictions_smote = model_logistic.predict(X_train_smote)
+test_predictions_smote = model_logistic.predict(X_test_scaled)
+train_accuracy_smote = accuracy_score(y_train_smote, train_predictions_smote)
+test_accuracy_smote = accuracy_score(y_test, test_predictions_smote)
+
+print("Logistic Regression after Oversampling")
+print("Train Accuracy:", train_accuracy_smote)
+print("Test Accuracy:", test_accuracy_smote)
+print(classification_report(y_test, test_predictions_smote))
+print("ROC-AUC:", roc_auc_score(y_test, model_logistic.predict_proba(X_test_scaled), multi_class='ovr'))
